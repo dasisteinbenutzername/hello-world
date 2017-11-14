d739f025dec7248c354df66b0349d467f52535f2
dasisteinbenutzername <33667214+dasisteinbenutzername@users.noreply.github.com>
2017-11-14 19:07:38 +0100

    Add files via upload
    blabla

---
+++ GitTesting/GitTesting/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace GitTesting
+{
+    partial class Test_from
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Test_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Test_button
+            // 
+            this.Test_button.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Test_button.Location = new System.Drawing.Point(289, 123);
+            this.Test_button.Name = "Test_button";
+            this.Test_button.Size = new System.Drawing.Size(304, 150);
+            this.Test_button.TabIndex = 0;
+            this.Test_button.Text = "Coooler Knopf";
+            this.Test_button.UseVisualStyleBackColor = false;
+            this.Test_button.Click += new System.EventHandler(this.Test_button_Click);
+            // 
+            // Test_from
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 457);
+            this.Controls.Add(this.Test_button);
+            this.Name = "Test_from";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Test_button;
+    }
+}
+
---
+++ GitTesting/GitTesting/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitTesting
+{
+    public partial class Test_from : Form
+    {
+        public Test_from()
+        {
+            InitializeComponent();
+        }
+
+        private void Test_button_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            Byte[] b = new Byte[3];
+            rnd.NextBytes(b);
+            Color color = Color.FromArgb(b[0], b[1], b[2]);
+            Test_button.BackColor = color;
+        }
+    }
+}
